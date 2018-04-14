eee1b5ae3cf507e993b88cc94ec58e5c6acf2cb5
Ahmet Faruk UNAL <ahmet@unalfaruk.com>
2018-04-14 19:03:40 +0300

    The sketch is rewritten to show new features.

--- Libraries/Teensy/examples/PressTempAlt/PressTempAlt.ino
+++ Libraries/Teensy/examples/PressTempAlt/PressTempAlt.ino
@@ -1,22 +1,37 @@
 /*
- *  BMP180    Teensy
- *  
+ *  BMP180    Teensy 3.5
+ *
+ *  For Wire,
  *  SDA       18
  *  SCL       19
- * 
+ *
+ *  For Wire1,
+ *  SDA       38
+ *  SCL       37
+ *
+ *
+ *  For Wire2,
+ *  SDA       4
+ *  SCL       3
+ *
+ *  For WireX,
+ *  You can see interfaces on the pinouts page: https://www.pjrc.com/teensy/pinout.html*
+ *
+ *
  */
-
 #include <Teensy_BMP180.h>
 double baseline;
 double bmpValues[2]; //0: Temperature, 1:Pressure
-Teensy_BMP180 bmp180;
+Teensy_BMP180 bmp180(&Wire);
+//Teensy_BMP180 bmp180(&Wire1); //If BMP180 is connected to Wire1 bus
+//Teensy_BMP180 bmp180(&Wire2); //If BMP180 is connected to Wire2 bus
 
 void setup() {
   bmp180.begin();
   if(getPressure()){
     baseline=bmpValues[1];
   }
-  
+
 }
 
 void loop() {
@@ -25,7 +40,7 @@ void loop() {
     Serial.print(bmpValues[0]);
     Serial.print(" C | ");
     Serial.print(bmpValues[1]);
-    Serial.print(" mb | ");    
+    Serial.print(" mb | ");
     Serial.print(altVal);
     Serial.println(" cm");
   }
